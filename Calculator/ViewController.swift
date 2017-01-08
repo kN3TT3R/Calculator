0599d3e4b448745b44f0ab436b313681991c4908
kN3TT3R <knetter@ptr-916yrbl8jp4st6yb1qx.18120a2.ip6.access.telenet.be>
2017-01-08 22:00:26 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kenneth Debruyn on 8/01/17.
+//  Copyright © 2017 kN3TT3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
